d3412cdfbb960ae368337b756aeff993c7423a8a
Lubrum <lucianobrum18@gmail.com>
2023-03-04 20:22:56 -0300

    Add actuator to spring

--- app/start.sh
+++ app/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar -Xms128M -Xmx128M -XX:PermSize=64m -XX:MaxPermSize=128m -Dspring.profiles.active=prod target/forum.jar
+/usr/lib/jvm/java-1.8.0-openjdk-amd64/bin/java -jar -Xms128M -Xmx128M -XX:PermSize=64m -XX:MaxPermSize=128m -Dspring.profiles.active=prod target/forum.jar
